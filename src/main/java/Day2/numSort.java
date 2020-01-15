8d311298737c4fb0311aafb93de0b7a34e3c5b99
haveagood@naver.com <haveagood@naver.com>
2020-01-15 20:40:43 +0900

    입출력 변경

--- src/main/java/Day2/numSort.java
+++ src/main/java/Day2/numSort.java
@@ -8,22 +8,25 @@ public class numSort {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = br.read();
+//        int num = br.read();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         int[] numCnt = new int[10001];
         for(int i=0;i<num;i++){
-            int temp_num = br.read();
+//            int temp_num = br.read();
+            int temp_num = sc.nextInt();
             numCnt[temp_num]++;
 
         }
-        br.close();
+
         for(int i=0;i<numCnt.length;i++){
             if(numCnt[i] > 0){
                 for(int j=0;j<numCnt[i];j++){
-                    bw.write(i);
+//                    bw.write(i);
+                    System.out.println(i);
                 }
 
             }
         }
-        bw.close();
     }
 }
